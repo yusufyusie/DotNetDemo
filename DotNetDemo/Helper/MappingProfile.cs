7dd61f114631d49a7df74c3f2fc6ad063f1092d9
yesuf023 <yesuffenta023@gmail.com>
2021-09-14 09:36:55 -0700

    yes

---
+++ DotNetDemo/Helper/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DataModel.DTO;
+using DataModel.Entity;
+
+namespace API.Helper
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Employee, CreateEmployeeDto>();
+            CreateMap<CreateEmployeeDto, Employee>();
+        }
+    }
+}
